f63683e4d35b31d592bbb50501522662b333b631
Sambit Kumar Mondal <sambitmondal02@gmail.com>
2024-03-14 15:07:24 +0530

    fixed collectible mechanics

--- Scenes/collectible.gd
+++ Scenes/collectible.gd
@@ -1,8 +1,7 @@
-extends StaticBody2D
+extends Area2D
 
-@onready var circlo = $"../circlo"
 
 func _on_body_entered(body):
 	# Check if the colliding body has the name "CharacterBody2D"
-	if body.name == circlo:
+	if body.name == "circlo":
 		queue_free()
